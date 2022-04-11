b3d0aaa7b40b75f3732104811c51dbab8324751b
g0bel1n <lucas.saban@icloud.com>
2022-04-11 18:44:40 +0200

    shut false positive pandas warning

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
  
 setup(
     name='TinyAutoML',
-    version='0.2.2.1',
-    packages=['TinyAutoML'],
+    version='0.2.2.2',
+    packages=['TinyAutoML', 'TinyAutoML.Preprocessing', 'TinyAutoML.constants', 'TinyAutoML.Models', 'TinyAutoML.support'],
     url='https://github.com/g0bel1n/TinyAutoML',
     license='MIT',
     author='g0bel1n',
@@ -16,7 +16,7 @@ setup(
                         'matplotlib',
                         'xgboost'],
     description='Combinaison of ML models for binary classification. Academic Project.',
-    download_url = 'https://github.com/g0bel1n/TinyAutoML/archive/refs/tags/v0.2.2.1.tar.gz',
+    download_url = 'https://github.com/g0bel1n/TinyAutoML/archive/refs/tags/v0.2.2.2.tar.gz',
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
