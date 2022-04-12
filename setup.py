72444a0128ee449c7eff93e953a183d469b1e362
g0bel1n <lucas.saban@icloud.com>
2022-04-12 15:40:22 +0200

    simplified comprehensiveSearch Inclusion and pool sharing

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
  
 setup(
     name='TinyAutoML',
-    version='0.2.2.7',
+    version='0.2.3',
     packages=['TinyAutoML', 'TinyAutoML.Preprocessing', 'TinyAutoML.constants', 'TinyAutoML.Models', 'TinyAutoML.support'],
-    url='https://github.com/g0bel1n/TinyAutoML',
+    url='https://github.com/g0bel1n/TinyAutoML/tree/pooling-opt',
     license='MIT',
     author='g0bel1n',
     author_email='lucas.saban@ensae.fr',
@@ -16,7 +16,7 @@ setup(
                         'matplotlib',
                         'xgboost'],
     description='Combinaison of ML models for binary classification. Academic Project.',
-    download_url = 'https://github.com/g0bel1n/TinyAutoML/archive/refs/tags/v0.2.2.7.tar.gz',
+    download_url = 'https://github.com/g0bel1n/TinyAutoML/archive/refs/tags/v0.2.3.tar.gz',
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
